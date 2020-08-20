9b37edcf61970bf6331c5ee4a90371186f4719b6
lookandhate <princessselestiapwnz@mail.ru>
2020-08-20 20:38:53 +0400

    Filled levelLocalizaton map with level names

--- AWRPC/src/main.cpp
+++ AWRPC/src/main.cpp
@@ -2,7 +2,67 @@
 #include "Windows.h"
 #include <tlhelp32.h>
 #include <tchar.h>
+
 #include <iostream>
+#include <string>
+#include <map>
+
+
+struct MapLocalization
+{
+	const std::string m_eng;
+	const std::string m_rus;
+	
+	MapLocalization() :
+		m_rus("NULL"), m_eng("NULL") {};
+	
+	MapLocalization(std::string eng, std::string rus) :
+		m_rus(rus), m_eng(eng) {};
+
+};
+
+std::map<std::string, MapLocalization> levelLocalization = {
+	{"cus_alabino_polygon" , MapLocalization("Alabino", "Алабино")},
+	{"gar_base" , MapLocalization("Hangar", "Ангар")},
+	{"gar_eeu_lean" , MapLocalization("Hangar", "Ангар")},
+	{"gar_geom_thumbnail" , MapLocalization("Hangar", "Ангар")},
+	{"gar_outdoor" , MapLocalization("Hangar", "Ангар")},
+	{"gar_season_02" , MapLocalization("Hangar", "Ангар")},
+	{"gar_season_03" , MapLocalization("Hangar", "Ангар")},
+	{"gar_season_04" , MapLocalization("Hangar", "Ангар")},
+	{"gar_season_05" , MapLocalization("Hangar", "Ангар")},
+	{"gar_china" , MapLocalization("Hangar", "Ангар")},
+	{"gar_china_spring_festival" , MapLocalization("Hangar", "Ангар")},
+	{"gar_china_unniversary_alabino_polygon" , MapLocalization("Hangar", "Ангар")},
+	{"gar_holiday_02" , MapLocalization("Hangar", "Ангар")},
+	{"gar_holiday_03" , MapLocalization("Hangar", "Ангар")},
+	{"gar_holiday04" , MapLocalization("Hangar", "Ангар")},
+	{"gar_holiday05" , MapLocalization("Hangar", "Ангар")},
+	{"gar_holiday06" , MapLocalization("Hangar", "Ангар")},
+	{"gar_holiday" , MapLocalization("Hangar", "Ангар")},
+	{"gar_nevada" , MapLocalization("Hangar", "Ангар")},
+
+	{"glo01_barrendivide" , MapLocalization("Barren Divide", "Ткварчели")},
+	{"glo05_ghostfield" , MapLocalization("Ghostfield", "Безмер")},
+	{"glo06_narrows" , MapLocalization("Narrows", "Кошице")},
+	{"glo07_roughneck" , MapLocalization("Roughneck", "Надым")},
+	{"glo08_china" , MapLocalization("Baise", "Байсэ")},
+	{"glo09_grassyfields" , MapLocalization("Pleternica", "Плетерница")},
+	{"glo11_alpine" , MapLocalization("Grindelwald", "Гриндельвальд")},
+	{"glo17_coruscant" , MapLocalization("Moscow", "Москва")},
+	{"pvp02_riverpoint" , MapLocalization("Cold Strike", "Гори")},
+	{"pvp05_pipelines" , MapLocalization("River Point", "Мостар")},
+	{"pvp06_portstorm" , MapLocalization("Pipelines", "Эль-Ариш")},
+	{"pvp07_coastalthreat" , MapLocalization("Port Storm", "Умм-Каср")},
+	{"pvp09_reactor" , MapLocalization("Coastal Threat", "Батуми")},
+	{"pvp13_desertcrossing" , MapLocalization("Reactor", "Уризар")},
+	{"pvp14_lostisland" , MapLocalization("Desert Crossing", "Суэцкий канал")},
+	{"pvp15_frontline" , MapLocalization("Lost Island", "Палау")},
+	{"pvp18_canyon" , MapLocalization("Front Line", "Виста")},
+	{"pvp19_tropicalcoast" , MapLocalization("Chemical Plant", "Аламо")},
+	{"pvp20_panamacanal" , MapLocalization("Tropical Coast", "Порт-Антонио")},
+	{"cus_alabino_polygon" , MapLocalization("Waterway", "Панамский канал")},
+};
 
 namespace MemUtils {
 	constexpr uintptr_t MAP_NAME_MEMORY_ADDRESS = 53356283;
@@ -148,6 +208,7 @@ int main()
 	}
 
 	char buffer[20] = "Nothing";
+	std::string level = "Nothing";
 
 	std::cout << "Game is running\n";
 
@@ -163,15 +224,24 @@ int main()
 	std::cout << "Handle Status:" << bHandleStatus << std::endl;
 
 
-	bool bMapRead = GetCurrentMap(gamehandle, &buffer, BaseAddress);
-	std::cout << "Is Map read: " << bMapRead << buffer << std::endl;
+	//bool bMapRead = GetCurrentMap(gamehandle, &buffer, BaseAddress);
+	//level = std::string(buffer);
+	//int slash_index = level.find('/');
+	//level = level.substr(0, slash_index);
+	//std::cout << "Is Map read: " << bMapRead << buffer << std::endl;
 
 	std::cin.get();
 	while (true)
 	{
 		system("cls");
 		bool bMapRead = GetCurrentMap(gamehandle, &buffer, BaseAddress);
-		std::cout << "Is Map read: " << bMapRead << " Buffer: " <<  buffer << std::endl;
+		
+		level = std::string(buffer);
+		int slash_index = level.find('/');
+		level = level.substr(0, slash_index);
+		std::string eulocalizedlevel = levelLocalization[level].m_eng;
+
+		std::cout << "Is Map read: " << bMapRead << " Level: " <<  level << std::endl << " Localized Level(ENG): " << levelLocalization[level].m_eng << std::endl << " Localized Level(RUS): " << levelLocalization[level].m_rus << std::endl;
 		Sleep(100);
 	}
 }
